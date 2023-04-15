124cbb0be65e0aebdd384f0e1eed9ed38c8b1769
bghira <bghira@users.github.com>
2023-04-15 16:44:52 -0700

    Fix threaded replies using the star
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_master/cogs/image/img2img.py
+++ discord_tron_master/cogs/image/img2img.py
@@ -44,7 +44,8 @@ class Img2img(commands.Cog):
         if in_my_thread and not message.attachments and message.content[0] != "!" and message.content[0] != "+":
             print("Attempting to run generate command?")
             generator = self.bot.get_cog('Generate')
-            prompt = message.content[:-1]
+            # Strip the first character:
+            prompt = message.content[1:]
             await generator.generate(message, prompt=prompt)
             return
 
