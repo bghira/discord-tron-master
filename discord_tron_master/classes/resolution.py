102993c6a2fd1dcff6d3bedd022b15022a0dd272
bghira <bghira@users.github.com>
2023-04-17 22:04:13 -0700

    More resolutions
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_master/classes/resolution.py
+++ discord_tron_master/classes/resolution.py
@@ -34,6 +34,7 @@ class ResolutionHelper:
         {"width": 1024, "height": 576, "scaling_factor": 40},
         {"width": 1280, "height": 720, "scaling_factor": 30},
         {"width": 1920, "height": 1080, "scaling_factor": 30},
+        {"width": 2160, "height": 1440, "scaling_factor": 30},
         {"width": 3840, "height": 2160, "scaling_factor": 30},
         {"width": 7680, "height": 4320, "scaling_factor": 30},
     ]
