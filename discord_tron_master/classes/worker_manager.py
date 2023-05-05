c407a52a475190388c91d45a29f88cc9c3b9cdeb
bghira <bghira@users.github.com>
2023-05-04 20:29:38 -0700

    More debugging/stablevicuna stuff
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_master/classes/worker_manager.py
+++ discord_tron_master/classes/worker_manager.py
@@ -14,6 +14,7 @@ class WorkerManager:
             "memory": [],
             "llama": [],
             "stablelm": [],
+            "stablevicuna": [],
             "tts_bark": [],
         }
         self.queue_manager = None
