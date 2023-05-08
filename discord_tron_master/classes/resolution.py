276511488ebd9fa88d68e5e9ee63fd92e9c80b34
bghira <bghira@users.github.com>
2023-05-08 11:27:09 -0700

    Reconcile resolution list on master/client
    Signed-off-by: bghira <bghira@users.github.com>

--- discord_tron_master/classes/resolution.py
+++ discord_tron_master/classes/resolution.py
@@ -1,44 +1,41 @@
 from discord_tron_master.classes.app_config import AppConfig
+
 config = AppConfig()
 
 
 class ResolutionHelper:
     resolutions = [
         # 1:1 aspect ratio
-        {"width": 128, "height": 128, "scaling_factor": 100},
-        {"width": 256, "height": 256, "scaling_factor": 88},
-        {"width": 512, "height": 512, "scaling_factor": 30},
-        {"width": 1024, "height": 1024, "scaling_factor": 30},
-        {"width": 2048, "height": 2048, "scaling_factor": 30},
-        {"width": 4096, "height": 4096, "scaling_factor": 30},
-
+        {"width": 128, "height": 128, "speed_factor": 100},
+        {"width": 256, "height": 256, "speed_factor": 100},
+        {"width": 512, "height": 512, "speed_factor": 100},
+        {"width": 768, "height": 768, "speed_factor": 30},
+        {"width": 1024, "height": 1024, "speed_factor": 30, "default_max": True},
+        {"width": 2048, "height": 2048, "speed_factor": 30},
+        {"width": 4096, "height": 4096, "speed_factor": 30},
         # 2:3 aspect ratio
-        {"width": 128, "height": 192, "scaling_factor": 80},
-        {"width": 256, "height": 384, "scaling_factor": 60},
-        {"width": 512, "height": 768, "scaling_factor": 49},
-        {"width": 1024, "height": 1536, "scaling_factor": 30},
-        {"width": 2048, "height": 3072, "scaling_factor": 30},
-        {"width": 4096, "height": 6144, "scaling_factor": 30},
-
+        {"width": 128, "height": 192, "speed_factor": 80},
+        {"width": 256, "height": 384, "speed_factor": 60},
+        {"width": 512, "height": 768, "speed_factor": 49},
+        {"width": 1024, "height": 1536, "speed_factor": 30, "default_max": True},
+        {"width": 2048, "height": 3072, "speed_factor": 30},
+        {"width": 4096, "height": 6144, "speed_factor": 30},
         # 3:2 aspect ratio
-        {"width": 192, "height": 128, "scaling_factor": 94},
-        {"width": 384, "height": 256, "scaling_factor": 76},
-        {"width": 768, "height": 512, "scaling_factor": 52},
-        {"width": 1536, "height": 1024, "scaling_factor": 30},
-        {"width": 3072, "height": 2048, "scaling_factor": 30},
-        {"width": 6144, "height": 4096, "scaling_factor": 30},
-
+        {"width": 192, "height": 128, "speed_factor": 94},
+        {"width": 384, "height": 256, "speed_factor": 76},
+        {"width": 768, "height": 512, "speed_factor": 52},
+        {"width": 1536, "height": 1024, "speed_factor": 30, "default_max": True},
+        {"width": 3072, "height": 2048, "speed_factor": 30},
+        {"width": 6144, "height": 4096, "speed_factor": 30},
         # 16:9 aspect ratio
-        {"width": 256, "height": 144, "scaling_factor": 40},
-        {"width": 512, "height": 288, "scaling_factor": 40},
-        {"width": 1024, "height": 576, "scaling_factor": 40},
-        {"width": 1280, "height": 720, "scaling_factor": 30},
-        {"width": 1920, "height": 1080, "scaling_factor": 30},
-        {"width": 2160, "height": 1440, "scaling_factor": 30},
-        {"width": 3840, "height": 2160, "scaling_factor": 30},
+        {"width": 256, "height": 144, "speed_factor": 40},
+        {"width": 512, "height": 288, "speed_factor": 40},
+        {"width": 1024, "height": 576, "speed_factor": 40},
+        {"width": 1280, "height": 720, "speed_factor": 30},
+        {"width": 1920, "height": 1080, "speed_factor": 30, "default_max": True},
+        {"width": 2160, "height": 1440, "speed_factor": 30},
+        {"width": 3840, "height": 2160, "speed_factor": 30},
     ]
-
-
 
     def is_valid_resolution(self, width, height):
         for res in ResolutionHelper.resolutions:
@@ -49,9 +46,7 @@ class ResolutionHelper:
         resolutions = ResolutionHelper.resolutions
         if resolution is not None:
             width, height = map(int, resolution.split("x"))
-            if any(
-                r["width"] == width and r["height"] == height for r in resolutions
-            ):
+            if any(r["width"] == width and r["height"] == height for r in resolutions):
                 return True
             else:
                 return False
@@ -69,7 +64,9 @@ class ResolutionHelper:
 
         # Sort resolution groups by width and height
         for ar, resolutions in grouped_resolutions.items():
-            grouped_resolutions[ar] = sorted(resolutions, key=lambda r: (r["width"], r["height"]))
+            grouped_resolutions[ar] = sorted(
+                resolutions, key=lambda r: (r["width"], r["height"])
+            )
 
         # Calculate the maximum number of rows for the table
         max_rows = max(len(resolutions) for resolutions in grouped_resolutions.values())
@@ -77,14 +74,29 @@ class ResolutionHelper:
         # Calculate the maximum field text width for each column, including the indicator
         max_field_widths = {}
         for ar, resolutions in grouped_resolutions.items():
-            max_field_widths[ar] = max(len(f"{r['width']}x{r['height']}") + 2 * indicator_length for r in resolutions)
+            max_field_widths[ar] = max(
+                len(f"{r['width']}x{r['height']}") + 2 * indicator_length
+                for r in resolutions
+            )
 
         # Generate resolution list in Markdown columns with padding
-        header_row = "| " + " | ".join(ar.ljust(max_field_widths[ar]) for ar in grouped_resolutions.keys()) + " |\n"
-        
+        header_row = (
+            "| "
+            + " | ".join(
+                ar.ljust(max_field_widths[ar]) for ar in grouped_resolutions.keys()
+            )
+            + " |\n"
+        )
+
         # Update the separator_row generation
-        separator_row = "+-" + "-+-".join("-" * (max_field_widths[ar]) for ar in grouped_resolutions.keys()) + "-+\n"
-        
+        separator_row = (
+            "+-"
+            + "-+-".join(
+                "-" * (max_field_widths[ar]) for ar in grouped_resolutions.keys()
+            )
+            + "-+\n"
+        )
+
         resolution_list = header_row + separator_row
 
         for i in range(max_rows):
@@ -116,6 +128,7 @@ class ResolutionHelper:
 
     def aspect_ratio(self, resolution_item: dict):
         from math import gcd
+
         width = resolution_item["width"]
         height = resolution_item["height"]
         # Calculate the greatest common divisor of width and height
@@ -127,4 +140,3 @@ class ResolutionHelper:
 
         # Return the aspect ratio as a string in the format "width:height"
         return f"{ratio_width}:{ratio_height}"
-
