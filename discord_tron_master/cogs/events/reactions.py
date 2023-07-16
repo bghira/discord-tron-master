57851f250c9bdb9d8d3820c49c477deb343d2616
bghira <bghira@users.github.com>
2023-07-15 19:49:07 -0700

    PNG info: fix last commit

--- discord_tron_master/cogs/events/reactions.py
+++ discord_tron_master/cogs/events/reactions.py
@@ -99,7 +99,7 @@ class Reactions(commands.Cog):
                 logging.debug(f'Would delete post: user_config {img.info["user_config"]}, prompt {img.info["prompt"]}')
                 await reaction.message.delete()
                 return
-        extra_params = { "user_config": new_config['user_config'], "user_id": user_id }
+        extra_params = { "user_config": new_config, "user_id": user_id }
         if reaction.emoji == '1️⃣':
             # We want to do an image variation, with the first image in the embeds.
             current_config = self.config.get_user_config(user.id)
