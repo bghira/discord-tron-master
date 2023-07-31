85b8d2f2e23a87534bba576c7162dd6e0c260c31
bghira <bghira@users.github.com>
2023-07-30 20:21:52 -0700

    Fix imports

--- discord_tron_master/cogs/user/user.py
+++ discord_tron_master/cogs/user/user.py
@@ -2,13 +2,6 @@ from discord.ext import commands
 from discord_tron_master.models.conversation import Conversations
 from discord_tron_master.classes.text_replies import return_random as random_fact
 from discord_tron_master.classes.app_config import AppConfig
-from discord_slash import cog_ext, SlashContext
-from discord_slash.utils.manage_commands import create_option, create_choice
-from discord_slash.model import SlashCommandOptionType
-from discord_slash.utils.manage_components import create_button, create_actionrow
-from discord_slash.model import ButtonStyle
-from discord_slash.utils.manage_components import wait_for_component
-
 
 import logging
 
