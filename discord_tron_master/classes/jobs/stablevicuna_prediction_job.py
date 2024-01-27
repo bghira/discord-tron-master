8d2020181dab5b83e8f2be8bcd6dea76dd8164d7
bghira <bghira@users.github.com>
2024-01-27 12:04:46 -0600

    fix typo

--- discord_tron_master/classes/jobs/stablevicuna_prediction_job.py
+++ discord_tron_master/classes/jobs/stablevicuna_prediction_job.py
@@ -4,7 +4,7 @@ from discord_tron_master.classes.app_config import AppConfig
 
 class StableVicunaPredictionJob(Job):
     def __init__(self, author_id: str, payload):
-        super().__init__("stablevicuna", "stablevicuna", "predict", author_id: str, payload)
+        super().__init__("stablevicuna", "stablevicuna", "predict", author_id, payload)
         self.date_created = time.time()
 
     async def format_payload(self):
