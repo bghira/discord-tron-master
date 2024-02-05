9092ea9cde6ce9585d9ca0efb61b133d11b7218f
bghira <bghira@users.github.com>
2024-02-04 21:44:13 -0600

    openai: update image gen text to use compel syntax

--- discord_tron_master/classes/openai/text.py
+++ discord_tron_master/classes/openai/text.py
@@ -60,20 +60,19 @@ class GPT:
         system_role = f"{system_role}digital artwork, feels like the first time, we went to the zoo, colourful and majestic, amazing clouds in the sky, epic\n"
         system_role = f"{system_role}The subject must come first, with actions coming next, and then style attributes.\n"
         system_role = f"{system_role}Any additional output other than the prompt will damage the results. Stick to just the prompts."
-        system_role = "You are a Stable Diffusion Prompt Generator Bot. Respond as one would"
         image_prompt_response = await self.turbo_completion(system_role, prompt, temperature=1.18)
         logging.debug(f'OpenAI returned the following response to the prompt: {image_prompt_response}')
-        # prompt_pieces = image_prompt_response.split(', ')
-        # logging.debug(f'Prompt pieces: {prompt_pieces}')
-        # # We want to turn the "foo, bar, buz" into ("foo", "bar", "buzz").and()
-        # prompt_output = "("
-        # for index, prompt_piece in enumerate(prompt_pieces):
-        #     if prompt_output == "(":
-        #         prompt_output = f'{prompt_output}"{prompt_piece}"'
-        #         continue
-        #     prompt_output = f'{prompt_output}, "{prompt_piece}"'
-        # prompt_output = f'{prompt_output}).and()'
-        return image_prompt_response
+        prompt_pieces = image_prompt_response.split(', ')
+        logging.debug(f'Prompt pieces: {prompt_pieces}')
+        # We want to turn the "foo, bar, buz" into ("foo", "bar", "buzz").and()
+        prompt_output = "("
+        for index, prompt_piece in enumerate(prompt_pieces):
+            if prompt_output == "(":
+                prompt_output = f'{prompt_output}"{prompt_piece}"'
+                continue
+            prompt_output = f'{prompt_output}, "{prompt_piece}"'
+        prompt_output = f'{prompt_output}).and()'
+        return prompt_output
 
     async def auto_model_select(self, prompt: str, query_str: str = None):
         if query_str is None:
