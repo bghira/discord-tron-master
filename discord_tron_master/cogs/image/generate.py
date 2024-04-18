8020ceafd181d28b73e7aff498cf4eca0849f2d2
bghira <bghira@users.github.com>
2024-04-18 08:16:36 -0600

    add sd3 api support (wip)

--- discord_tron_master/cogs/image/generate.py
+++ discord_tron_master/cogs/image/generate.py
@@ -99,7 +99,7 @@ class Generate(commands.Cog):
             prompt = prompt.replace('--sd3', '').strip()
             try:
                 image = stabilityai.generate_image(prompt)
-                await ctx.send(file=discord.File(image, "image.png"))
+                await ctx.channel.send(file=discord.File(image, "image.png"))
             except Exception as e:
                 await ctx.send(f"Error generating image: {e}")
         elif prompt == 'unconditional' or prompt == 'blank':
