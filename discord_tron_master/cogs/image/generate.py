9205f3b3dbd34190fcb84adcb4a60d50740575e6
bghira <bghira@users.github.com>
2024-04-18 10:15:43 -0600

    fix text

--- discord_tron_master/cogs/image/generate.py
+++ discord_tron_master/cogs/image/generate.py
@@ -98,12 +98,20 @@ class Generate(commands.Cog):
             from PIL import Image
             dalle_image = Image.open(dalle_image)
             # Add "DALLE-3" and "Stable Diffusion 3" labels to upper left corner
-            from PIL import ImageDraw
+            from PIL import ImageDraw, ImageFont
+            try:
+                # Attempt to use a specific font
+                font = ImageFont.truetype(
+                    "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 40
+                )
+            except IOError:
+                # Fallback to default font
+                font = ImageFont.load_default(size=40)
             sd3_image = Image.open(sd3_image)
             draw = ImageDraw.Draw(sd3_image)
-            draw.text((10, 10), "Stable Diffusion 3", (255, 255, 255))
+            draw.text((10, 10), "Stable Diffusion 3", (255, 255, 255), font=font, stroke_width=4)
             draw = ImageDraw.Draw(dalle_image)
-            draw.text((10, 10), "DALL-E", (255, 255, 255))
+            draw.text((10, 10), "DALL-E", (255, 255, 255), font=font, stroke_width=4)
             width, height = dalle_image.size
             new_image = Image.new('RGB', (width * 2, height))
             new_image.paste(sd3_image, (0, 0))
